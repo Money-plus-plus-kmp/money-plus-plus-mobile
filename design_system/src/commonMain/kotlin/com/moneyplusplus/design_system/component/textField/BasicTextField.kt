67f4266bc755464514a138d61e6f56e749540e75
Youssef Mohamed <youssef.mu.saber@gmail.com>
2026-01-18 17:33:16 +0200

    implemented the text field

---
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/component/textField/BasicTextField.kt
@@ -0,0 +1,227 @@
+package com.moneyplusplus.design_system.component.textField
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.moneyplusplus.design_system.component.icon.Icon
+import com.moneyplusplus.design_system.component.text.Text
+import com.moneyplusplus.design_system.theme.theme.Theme
+
+@Composable
+fun BasicTextField(
+    value: String,
+    hint: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    leadingContent: @Composable (() -> Unit)? = null,
+    leadingIcon: Painter? = null,
+    trailingIcon: Painter? = null,
+    leadingIconTint: Color = Theme.colorScheme.body,
+    singleLine: Boolean = true,
+    minLines: Int = 1,
+    maxLines: Int = Int.MAX_VALUE,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    isError: Boolean = false,
+    showTrailingDivider: Boolean = true,
+    errorMessage: String? = null,
+    shape: Shape = RoundedCornerShape(16.dp),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    focusRequester: FocusRequester = FocusRequester(),
+    onFocusChanged: (Boolean) -> Unit = {},
+    onTrailingIconClick: (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    maxCharacters: Int = Int.MAX_VALUE,
+) {
+    Column(modifier) {
+
+        Row(Modifier.fillMaxWidth()) {
+            leadingContent?.let {
+                leadingContent()
+                Spacer(Modifier.width(4.dp))
+            }
+
+            BasicTextField(
+                value = value,
+                onValueChange = {
+                    if (it.length <= maxCharacters)
+                        onValueChanged(it)
+                },
+                enabled = enabled,
+                readOnly = readOnly,
+                minLines = minLines,
+                maxLines = if (singleLine) 1 else maxLines,
+                textStyle = Theme.typography.body.small.copy(
+                    color = Theme.colorScheme.title
+                ),
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                singleLine = singleLine,
+                cursorBrush = SolidColor(Theme.colorScheme.primary.primary),
+                decorationBox = { innerTextField ->
+                    TextFieldContent(
+                        innerTextField = innerTextField,
+                        text = value,
+                        isError = isError,
+                        singleLine = singleLine,
+                        hint = hint,
+                        leadingIcon = leadingIcon,
+                        trailingIcon = trailingIcon,
+                        onTrailingIconClick = onTrailingIconClick,
+                        showTrailingDivider = showTrailingDivider,
+                        leadingIconTint = leadingIconTint
+                    )
+                },
+                visualTransformation = visualTransformation,
+                modifier = Modifier
+                    .weight(1f)
+                    .clip(shape)
+                    .background(color = Theme.colorScheme.surface.surfaceLow)
+                    .focusRequester(focusRequester)
+                    .onFocusChanged {
+                        onFocusChanged(it.isFocused)
+                    }
+            )
+        }
+
+        errorMessage?.let {
+            Text(
+                text = errorMessage,
+                style = Theme.typography.label.small,
+                modifier = Modifier.padding(
+                    start = 12.dp,
+                    top = 2.dp
+                ),
+                color = Theme.colorScheme.accent.red.red,
+            )
+        }
+    }
+}
+
+@Composable
+private fun TextFieldContent(
+    innerTextField: @Composable () -> Unit,
+    text: String,
+    hint: String,
+    leadingIcon: Painter?,
+    trailingIcon: Painter?,
+    leadingIconTint: Color,
+    isError: Boolean,
+    singleLine: Boolean,
+    showTrailingDivider: Boolean = true,
+    onTrailingIconClick: (() -> Unit)? = null,
+) {
+
+    val animatedIconErrorColor by animateColorAsState(
+        targetValue = if (isError) Theme.colorScheme.accent.red.red else leadingIconTint
+
+    )
+    Row(
+        modifier = Modifier.padding(12.dp).fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        leadingIcon?.let {
+
+            Icon(
+                painter = leadingIcon,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(24.dp),
+                tint = animatedIconErrorColor
+            )
+        }
+
+        InnerTextFieldWithHint(
+            innerTextField = innerTextField,
+            text = text,
+            hint = hint,
+            singleLine = singleLine,
+            modifier = Modifier.weight(1f)
+        )
+
+        trailingIcon?.let {
+            if (showTrailingDivider)
+                VerticalDivider()
+            Image(
+                painter = trailingIcon,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable(
+                        enabled = onTrailingIconClick != null,
+                        indication = null,
+                        interactionSource = remember {
+                            MutableInteractionSource()
+                        }
+                    ) {
+                        onTrailingIconClick?.invoke()
+                    }
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun InnerTextFieldWithHint(
+    innerTextField: @Composable (() -> Unit),
+    text: String,
+    hint: String,
+    singleLine: Boolean,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = if (singleLine) Alignment.CenterStart else Alignment.TopStart
+    ) {
+        innerTextField()
+        if (text.isEmpty()) {
+            Text(
+                text = hint,
+                style = Theme.typography.label.medium,
+                color = Theme.colorScheme.title,
+            )
+        }
+    }
+}
+
+@Composable
+private fun VerticalDivider() {
+    Box(
+        Modifier
+            .padding(horizontal = 8.dp)
+            .size(1.dp, 21.dp)
+            .background(Theme.colorScheme.stroke),
+    )
+}
