9d97671bb0228ff7a493a56d5f626f51fe32c8d4
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-01-25 15:18:37 +0200

    feat: add domain module for Koin dependency injection

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -10,7 +10,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-
+            implementation(libs.koin.core)
         }
         commonTest.dependencies {
                     // Core multiplatform test
---
+++ domain/src/commonMain/kotlin/com/moneyplusplus/domain/di/DomainModule.kt
@@ -0,0 +1,13 @@
+package com.moneyplusplus.domain.di
+
+import com.moneyplusplus.domain.usecase.validation.EmailValidator
+import com.moneyplusplus.domain.usecase.validation.NameValidator
+import com.moneyplusplus.domain.usecase.validation.PasswordValidator
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+
+val domainModule = module {
+    singleOf(::EmailValidator)
+    singleOf(::NameValidator)
+    singleOf(::PasswordValidator)
+}
