d8707754765bb48c68d560a9be715d6020d8cadf
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-01-25 15:03:33 +0200

    refactor: Replace custom TopAppBar with design system `AppBar` component

--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/feature/forgetPassword/component/topAppBar.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/feature/forgetPassword/component/topAppBar.kt
@@ -1,21 +1,13 @@
 package com.moneyplusplus.presentation.feature.forgetPassword.component
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import com.moneyplusplus.design_system.component.appBar.AppBar
+import com.moneyplusplus.design_system.component.icon.Icon
+import com.moneyplusplus.design_system.component.image.Image
 import com.moneyplusplus.design_system.theme.theme.Theme
 import money.presentation.generated.resources.Res
 import money.presentation.generated.resources.arrow_left
@@ -27,41 +19,29 @@ import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun TopAppBar(
+    onBackClick: () -> Unit,
     modifier: Modifier = Modifier
-){
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(Theme.colorScheme.surface.surfaceLow)
-            .padding(vertical = 12.dp, horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            painter = painterResource(Res.drawable.arrow_left_04),
-            contentDescription = stringResource(Res.string.arrow_left),
-            modifier = Modifier
-                .padding(end = 8.dp)
-                .clip(CircleShape)
-                .background(Theme.colorScheme.surface.surfaceHigh)
-                .padding(10.dp)
-                .size(20.dp)
-        )
-
-
-        Text(
-            text = stringResource(Res.string.forget_password),
-            style = Theme.typography.title.small,
-            color = Theme.colorScheme.title
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Image(
-            painter = painterResource(Res.drawable.money),
-            contentDescription = stringResource(Res.string.money),
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .size(width = 65.dp, height = 25.dp)
-        )
-    }
+) {
+    AppBar(
+        modifier = modifier,
+        title = stringResource(Res.string.forget_password),
+        titleColor = Theme.colorScheme.title,
+        leadingContent = {
+            Icon(
+                painter = painterResource(Res.drawable.arrow_left_04),
+                contentDescription = stringResource(Res.string.arrow_left),
+                modifier = Modifier.size(20.dp)
+            )
+        },
+        onLeadingClick = onBackClick,
+        trailingContent = {
+            Image(
+                painter = painterResource(Res.drawable.money),
+                contentDescription = stringResource(Res.string.money),
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(width = 65.dp, height = 25.dp)
+            )
+        }
+    )
 }
