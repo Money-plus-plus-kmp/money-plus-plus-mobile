7ebf4caaa7792dd891c1a0c1f91d70296ed9e2ea
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-01-27 11:14:32 +0200

    feat: Create GetTransactionsUseCase

---
+++ domain/src/commonMain/kotlin/com/moneyplusplus/domain/usecase/transaction/GetTransactionsUseCase.kt
@@ -0,0 +1,15 @@
+package com.moneyplusplus.domain.usecase.transaction
+
+import com.moneyplusplus.domain.entity.Transaction
+import com.moneyplusplus.domain.model.TransactionFilter
+import com.moneyplusplus.domain.repository.TransactionRepository
+
+class GetTransactionsUseCase(
+    private val transactionRepository: TransactionRepository
+
+) {
+    suspend operator fun invoke(transactionFilter: TransactionFilter): Result<List<Transaction>> =
+        transactionRepository.getTransactions(transactionFilter).map { transactions ->
+            transactions.sortedByDescending { it.date }
+        }
+}
