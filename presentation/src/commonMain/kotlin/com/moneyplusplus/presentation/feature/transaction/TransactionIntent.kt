5ef9e9e94a2d34778d4f7deff51905103b309d2a
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-01-28 15:59:25 +0200

    feat(transaction): Add filter by category and add transaction functionality

--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/feature/transaction/TransactionIntent.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/feature/transaction/TransactionIntent.kt
@@ -4,9 +4,13 @@ import com.moneyplusplus.presentation.base.UiIntent
 import kotlinx.datetime.LocalDate
 
 sealed class TransactionIntent : UiIntent {
-    object OnFilterClick : TransactionIntent()
+    data object OnFilterClick : TransactionIntent()
+    data class OnApplyFilterClick(val selectedCategoryIds: List<String>) : TransactionIntent()
     data object OnFilterSheetDismissed : TransactionIntent()
-    object OnDateClick : TransactionIntent()
+    data object OnDateClick : TransactionIntent()
     data class OnDateSelected(val date: LocalDate) : TransactionIntent()
     data class OnTransactionTypeClick(val type: TransactionTypeFilter) : TransactionIntent()
+    data object OnAddTransactionClick : TransactionIntent()
+    data object OnAddTransactionSheetDismissed : TransactionIntent()
+
 }
--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/feature/transaction/TransactionUiState.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/feature/transaction/TransactionUiState.kt
@@ -1,16 +1,19 @@
 package com.moneyplusplus.presentation.feature.transaction
 
 import com.moneyplusplus.presentation.base.UiState
+import com.moneyplusplus.presentation.model.CategoryUiModel
 import com.moneyplusplus.presentation.model.TransactionUiModel
 
-data class TransactionUiState (
+data class TransactionUiState(
     val transactions: List<TransactionUiModel> = emptyList(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String? = null,
     val typeFilter: TransactionTypeFilter = TransactionTypeFilter.ALL,
     val date: String = "",
-    val showFilterBottomSheet: Boolean = false,
-): UiState
-
-
+    val showCategoriesFilterBottomSheet: Boolean = false,
+    val showAddTransactionBottomSheet: Boolean = false,
+    val categories: List<CategoryUiModel> = emptyList(),
+    val selectedCategoryIds: List<String> = emptyList(),
+    val showDatePicker: Boolean = false
+) : UiState
