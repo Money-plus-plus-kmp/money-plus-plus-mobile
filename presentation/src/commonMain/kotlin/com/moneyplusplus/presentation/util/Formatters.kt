b894f7109483a87dbddf273460f36dcf22555392
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-01-28 15:55:12 +0200

    refactor(presentation): Improve amount formatting logic

--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/util/Formatters.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/util/Formatters.kt
@@ -10,11 +10,19 @@ fun LocalDateTime.formatDate(): String {
 }
 
 fun Double.formatAmount(): String {
-    val number = if (this % 1.0 == 0.0) {
+    val isInteger = this % 1.0 == 0.0
+    val text = if (isInteger) {
         this.toLong().toString()
     } else {
         this.toString()
     }
+
+    val parts = text.split(".")
+    val integerPart = parts[0]
+    val fractionalPart = if (parts.size > 1) ".${parts[1]}" else ""
+
     val regex = "(\\d)(?=(\\d{3})+$)".toRegex()
-    return number.replace(regex, "$1,")
+    val formattedInteger = integerPart.replace(regex, "$1,")
+
+    return formattedInteger + fractionalPart
 }
