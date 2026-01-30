6f3e7fd90d3150de58b0543a844330036b0721ea
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-01-31 00:26:44 +0200

    refactor: Simplify repository DI module syntax

--- data/src/commonMain/kotlin/com/moneyplusplus/data/di/repositoryModule.kt
+++ data/src/commonMain/kotlin/com/moneyplusplus/data/di/repositoryModule.kt
@@ -3,15 +3,16 @@ package com.moneyplusplus.data.di
 import com.moneyplusplus.data.repository.AuthRepositoryImpl
 import com.moneyplusplus.data.repository.FakeTransactionRepository
 import com.moneyplusplus.data.repository.fakedata.FakeCategoryRepository
+import com.moneyplusplus.domain.repository.AuthRepository
 import com.moneyplusplus.domain.repository.CategoryRepository
 import com.moneyplusplus.domain.repository.TransactionRepository
-import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 
-internal val repositoryModule = module {
-    singleOf(::AuthRepositoryImpl)
-    singleOf(::FakeTransactionRepository) { bind<TransactionRepository>() }
-    singleOf(::FakeCategoryRepository) { bind<CategoryRepository>() }
+val repositoryModule = module {
+    singleOf(::AuthRepositoryImpl) bind AuthRepository::class
+    singleOf(::FakeTransactionRepository) bind TransactionRepository::class
+    singleOf(::FakeCategoryRepository) bind CategoryRepository::class
 }
