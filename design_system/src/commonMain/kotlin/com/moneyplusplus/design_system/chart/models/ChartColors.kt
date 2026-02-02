5167f4d28d209d82cce8a945b49db62f74024e46
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 18:50:17 +0200

    feat: Introduce ChartColors data class for chart styling

---
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/models/ChartColors.kt
@@ -0,0 +1,11 @@
+package com.moneyplusplus.design_system.chart.models
+
+import androidx.compose.ui.graphics.Color
+
+data class ChartColors(
+    val lineColor: Color,
+    val gridColor: Color,
+    val axisLabelColor: Color,
+    val tooltipBackground: Color,
+    val tooltipTextColor: Color
+)
