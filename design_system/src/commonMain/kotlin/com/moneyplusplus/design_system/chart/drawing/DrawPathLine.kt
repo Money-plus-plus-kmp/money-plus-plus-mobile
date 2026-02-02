88283c1060c39e22f5b15d0cc58edd056ce86eb4
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 17:27:57 +0200

    feat: Add drawPathLineWrapper function for rendering animated path lines

---
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/drawing/DrawPathLine.kt
@@ -0,0 +1,29 @@
+package com.moneyplusplus.design_system.chart.drawing
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipRect
+import androidx.compose.ui.unit.dp
+import com.moneyplusplus.design_system.chart.models.LineParameters
+
+internal fun DrawScope.drawPathLineWrapper(
+    lineParameter: LineParameters,
+    strokePath: Path,
+    animatedProgress: Animatable<Float, AnimationVector1D>,
+    function: (LineParameters, Int) -> Unit,
+) {
+    lineParameter.data.indices.forEach { index ->
+        function(lineParameter, index)
+    }
+    clipRect(right = size.width * animatedProgress.value) {
+        drawPath(
+            path = strokePath, color = lineParameter.lineColor, style = Stroke(
+                width = 3.dp.toPx(), cap = StrokeCap.Round
+            )
+        )
+    }
+}
