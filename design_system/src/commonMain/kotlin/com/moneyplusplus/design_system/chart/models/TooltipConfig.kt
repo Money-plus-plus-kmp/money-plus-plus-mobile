5bb54acb08a21f932ccced4b55015b73f5157b7d
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 18:53:33 +0200

    refactor: Add valueSuffix to TooltipConfig for enhanced customization

--- design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/models/TooltipConfig.kt
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/models/TooltipConfig.kt
@@ -5,5 +5,6 @@ import androidx.compose.ui.graphics.Color
 data class TooltipConfig(
     val enabled: Boolean = true,
     val backgroundColor: Color = Color.Black,
-    val textColor: Color = Color.White
+    val textColor: Color = Color.White,
+    val valueSuffix: String = ""
 )
