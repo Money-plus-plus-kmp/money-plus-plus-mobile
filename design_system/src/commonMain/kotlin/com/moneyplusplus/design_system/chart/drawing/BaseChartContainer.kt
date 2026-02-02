029922c876cc826795dd391e22ca625fe533ac97
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 17:27:12 +0200

    feat: Implement baseChartContainer function for drawing chart components

---
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/drawing/BaseChartContainer.kt
@@ -0,0 +1,51 @@
+package com.moneyplusplus.design_system.chart.drawing
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+
+@OptIn(ExperimentalTextApi::class)
+internal fun <T> DrawScope.baseChartContainer(
+    xAxisData: List<T>,
+    textMeasure: TextMeasurer,
+    upperValue: Float,
+    lowerValue: Float,
+    gridColor: Color,
+    backgroundLineWidth: Float,
+    showGridWithSpacer: Boolean,
+    spacingY: Dp,
+    yAxisStyle: TextStyle,
+    xAxisStyle: TextStyle,
+    yAxisRange: Int,
+    xRegionWidth: Dp
+) {
+    xAxisDrawing(
+        xAxisData = xAxisData,
+        textMeasure = textMeasure,
+        xAxisStyle = xAxisStyle,
+        upperValue = upperValue,
+        xRegionWidth = xRegionWidth
+    )
+
+    yAxisDrawing(
+        upperValue = upperValue,
+        lowerValue = lowerValue,
+        textMeasure = textMeasure,
+        spacing = spacingY,
+        yAxisStyle = yAxisStyle,
+        yAxisRange = yAxisRange
+    )
+
+    grid(
+        gridColor = gridColor,
+        backgroundLineWidth = backgroundLineWidth,
+        showGridWithSpacer = showGridWithSpacer,
+        spacingY = spacingY,
+        yAxisRange = yAxisRange,
+        upperValue = upperValue,
+        textMeasurer = textMeasure
+    )
+}
