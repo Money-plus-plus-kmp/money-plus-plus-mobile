999cb3aaec506b26f255f884bcf2a5dfcf5f844c
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 17:08:32 +0200

    docs: Add KDoc to ChartPoint data class

--- design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/data/ChartPoint.kt
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/data/ChartPoint.kt
@@ -2,6 +2,9 @@ package com.moneyplusplus.design_system.chart.data
 
 import kotlinx.datetime.LocalDate
 
+/**
+ * Represents a single data point on the chart
+ */
 data class ChartPoint(
     val date: LocalDate,
     val value: Double
