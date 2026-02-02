223e51ace6d5b10319b9866a4df4b5c9f2b6880b
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 18:50:28 +0200

    refactor: Simplify ChartConfig by removing unused properties

--- design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/config/ChartConfig.kt
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/config/ChartConfig.kt
@@ -2,21 +2,10 @@ package com.moneyplusplus.design_system.chart.config
 
 import com.moneyplusplus.design_system.chart.models.TooltipConfig
 
-/**
- * Main configuration combining all chart settings
- */
 data class ChartConfig(
-    // colors removed
     val dimensions: ChartDimensions = ChartDimensions.Default,
     val tooltip: TooltipConfig = TooltipConfig(enabled = true),
-    val showGrid: Boolean = true,
-    val showYAxisLabels: Boolean = true,
-    val showXAxisLabels: Boolean = true,
-    val smoothLine: Boolean = true,
-    val showGradient: Boolean = true,
-    val yAxisLabelCount: Int = 5,
     val animationEnabled: Boolean = true,
-    val animationDuration: Int = 1000
 ) {
     companion object {
         val Default = ChartConfig()
