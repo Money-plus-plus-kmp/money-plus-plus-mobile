aef7461c29be4aa442173b7ef596eaebecf52448
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 17:48:32 +0200

    feat: Add TooltipConfig data class for customizable tooltip settings

---
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/models/TooltipConfig.kt
@@ -0,0 +1,9 @@
+package com.moneyplusplus.design_system.chart.models
+
+import androidx.compose.ui.graphics.Color
+
+data class TooltipConfig(
+    val enabled: Boolean = true,
+    val backgroundColor: Color = Color.Black,
+    val textColor: Color = Color.White
+)
