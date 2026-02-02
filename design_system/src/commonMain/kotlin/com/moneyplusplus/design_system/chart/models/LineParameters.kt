73cb4d63b959c47ab2ed8085bb62aed0daaa3c52
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-02 17:28:33 +0200

    feat: Add LineParameters data class for chart line configuration

---
+++ design_system/src/commonMain/kotlin/com/moneyplusplus/design_system/chart/models/LineParameters.kt
@@ -0,0 +1,11 @@
+package com.moneyplusplus.design_system.chart.models
+
+import androidx.compose.ui.graphics.Color
+
+data class LineParameters(
+    val label: String,
+    val data: List<Double>,
+    val lineColor: Color,
+    val lineShadow: Boolean,
+    val tooltipConfig: TooltipConfig = TooltipConfig(),
+)
