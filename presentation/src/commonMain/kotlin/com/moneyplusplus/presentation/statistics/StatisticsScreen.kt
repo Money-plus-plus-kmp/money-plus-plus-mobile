423f354608020a0eb6228b1cbb2c4f38e4c022be
abdo-essam <abdo.e.mosa@gmail.com>
2026-02-05 22:40:24 +0200

    refactor(statistics): Update dummy chart data
    Refactor StatisticsScreen.kt to update the hardcoded spending trend chart
    points with a new set of data for better representation.

--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/statistics/StatisticsScreen.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/statistics/StatisticsScreen.kt
@@ -24,67 +24,26 @@ fun StatisticsScreen() {
 
         SpendingTrendChart(
             title = "Spending Trend", points = listOf(
-                ChartPoint(
-                    LocalDate(2024, 12, 1), 500.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 2), 705.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 3), 60.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 4), 815.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 5), 90.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 6), 1501.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 7), 2010.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 8), 1100.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 9), 120.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 10), 100.0
-                ),
-
-                ChartPoint(
-                    LocalDate(2024, 12, 11), 550.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 12), 765.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 13), 608.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 14), 855.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 15), 960.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 16), 1520.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 17), 2040.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 18), 1860.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 19), 1290.0
-                ),
-                ChartPoint(
-                    LocalDate(2024, 12, 20), 1070.0
-                ),
+                ChartPoint(LocalDate(2024, 12, 1), 500.0),
+                ChartPoint(LocalDate(2024, 12, 2), 705.0),
+                ChartPoint(LocalDate(2024, 12, 3), 60.0),
+                ChartPoint(LocalDate(2024, 12, 4), 815.0),
+                ChartPoint(LocalDate(2024, 12, 5), 90.0),
+                ChartPoint(LocalDate(2024, 12, 6), 1501.0),
+                ChartPoint(LocalDate(2024, 12, 7), 2010.0),
+                ChartPoint(LocalDate(2024, 12, 8), 1100.0),
+                ChartPoint(LocalDate(2024, 12, 9), 120.0),
+                ChartPoint(LocalDate(2024, 12, 10), 100.0),
+                ChartPoint(LocalDate(2024, 12, 11), 550.0),
+                ChartPoint(LocalDate(2024, 12, 12), 765.0),
+                ChartPoint(LocalDate(2024, 12, 13), 608.0),
+                ChartPoint(LocalDate(2024, 12, 14), 855.0),
+                ChartPoint(LocalDate(2024, 12, 15), 960.0),
+                ChartPoint(LocalDate(2024, 12, 16), 1520.0),
+                ChartPoint(LocalDate(2024, 12, 17), 2040.0),
+                ChartPoint(LocalDate(2024, 12, 18), 1860.0),
+                ChartPoint(LocalDate(2024, 12, 19), 1290.0),
+                ChartPoint(LocalDate(2024, 12, 20), 1070.0),
             ), valueSuffix = "EG"
         )
     }
