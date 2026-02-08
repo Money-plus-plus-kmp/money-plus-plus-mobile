85507db234346fd47652ad41bd9be09f967964f8
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-08 18:59:48 +0200

    refactor: Relocate UI mappers to dedicated package

--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/model/CategoryUiModel.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/model/CategoryUiModel.kt
@@ -1,13 +1,6 @@
 package com.moneyplusplus.presentation.model
 
-import com.moneyplusplus.domain.entity.Category
-
 data class CategoryUiModel(
     val id: String,
     val name: String,
-)
-
-fun Category.toUiModel() = CategoryUiModel(
-    id = id.toString(),
-    name = name
 )
--- presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/model/TransactionUiModel.kt
+++ presentation/src/commonMain/kotlin/com/moneyplusplus/presentation/model/TransactionUiModel.kt
@@ -1,10 +1,5 @@
 package com.moneyplusplus.presentation.model
 
-import com.moneyplusplus.domain.entity.Transaction
-import com.moneyplusplus.domain.entity.TransactionType
-import com.moneyplusplus.presentation.formater.formatAmount
-import com.moneyplusplus.presentation.formater.formatDate
-
 data class TransactionUiModel(
     val id: String,
     val title: String,
@@ -14,12 +9,3 @@ data class TransactionUiModel(
     val categoryId: String
 )
 
-fun Transaction.toUiModel() =
-    TransactionUiModel(
-        id = id.toString(),
-        title = title,
-        amount = "${amount.formatAmount()} ${currency.code}",
-        isExpense = type == TransactionType.EXPENSE,
-        dateFormated = date.formatDate(),
-        categoryId = category.id.toString()
-    )
