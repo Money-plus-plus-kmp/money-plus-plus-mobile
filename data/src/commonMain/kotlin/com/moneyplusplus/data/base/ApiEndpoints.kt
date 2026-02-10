35594ede5bd9fc8eed6034a3c363036f9b08b924
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-02-10 18:50:32 +0200

    refactor(auth): Update authentication API endpoint paths

--- data/src/commonMain/kotlin/com/moneyplusplus/data/base/ApiEndpoints.kt
+++ data/src/commonMain/kotlin/com/moneyplusplus/data/base/ApiEndpoints.kt
@@ -2,8 +2,8 @@ package com.moneyplusplus.data.base
 
 object ApiEndpoints {
     const val LOGIN = "/auth/login"
-    const val REGISTER = "/register"
-    const val FORGOT_PASSWORD = "/forgot-password"
-    const val TOKEN_VALIDATION = "/token-validation"
+    const val REGISTER = "/auth/register"
+    const val FORGOT_PASSWORD = "/auth/forgot-password"
+    const val TOKEN_VALIDATION = "/auth/google"
 
 }
