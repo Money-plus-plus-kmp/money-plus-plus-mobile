a0462ba6381a9048420473a99443f3662b310792
bilalazzam <blalazam065@gmail.com>
2026-02-10 20:57:55 +0200

    add base url to ios app

--- data/src/iosMain/kotlin/com/moneyplusplus/data/di/networkModule.ios.kt
+++ data/src/iosMain/kotlin/com/moneyplusplus/data/di/networkModule.ios.kt
@@ -1,5 +1,8 @@
 package com.moneyplusplus.data.di
 
+import platform.Foundation.NSBundle
+
 actual fun getBaseUrl(): String {
-    TODO("Not yet implemented")
+    return NSBundle.mainBundle.objectForInfoDictionaryKey("BASE_URL") as? String
+        ?: error("BASE_URL not found in Info.plist")
 }
