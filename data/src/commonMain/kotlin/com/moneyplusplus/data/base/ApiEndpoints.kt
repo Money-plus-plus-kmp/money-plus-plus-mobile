ee78faae475a64d83d634e1259e7ccf7a26464c3
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-02-10 18:12:18 +0200

    feat: Define authentication API endpoints

---
+++ data/src/commonMain/kotlin/com/moneyplusplus/data/base/ApiEndpoints.kt
@@ -0,0 +1,9 @@
+package com.moneyplusplus.data.base
+
+object ApiEndpoints {
+    const val LOGIN = "/auth/login"
+    const val REGISTER = "/register"
+    const val FORGOT_PASSWORD = "/forgot-password"
+    const val TOKEN_VALIDATION = "/token-validation"
+
+}
