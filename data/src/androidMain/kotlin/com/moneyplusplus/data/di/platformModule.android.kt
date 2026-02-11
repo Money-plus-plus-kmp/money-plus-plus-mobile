9c5d3e2074804f19c702293241c265faace75d65
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-02-11 19:22:46 +0200

    refactor(data): Bind GoogleAuthManager to GoogleAuthDataSource

--- data/src/androidMain/kotlin/com/moneyplusplus/data/di/platformModule.android.kt
+++ data/src/androidMain/kotlin/com/moneyplusplus/data/di/platformModule.android.kt
@@ -1,10 +1,12 @@
 package com.moneyplusplus.data.di
 
 import com.moneyplusplus.data.auth.GoogleAuthManager
+import com.moneyplusplus.data.datasource.remote.auth.google.GoogleAuthDataSource
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal actual val platformModule: Module = module {
-    singleOf(::GoogleAuthManager)
+    singleOf(::GoogleAuthManager) bind GoogleAuthDataSource::class
 }
