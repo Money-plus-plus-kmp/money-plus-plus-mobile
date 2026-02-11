5e6ec834579a44fc3f91b87b541ec9c909ca58f5
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-02-11 19:23:11 +0200

    refactor(data): Rename LoginDto to LoginRequest

--- data/src/commonMain/kotlin/com/moneyplusplus/data/datasource/remote/request/LoginRequest.kt
+++ data/src/commonMain/kotlin/com/moneyplusplus/data/datasource/remote/request/LoginRequest.kt
@@ -1,9 +1,9 @@
-package com.moneyplusplus.data.datasource.remote.dto
+package com.moneyplusplus.data.datasource.remote.request
 
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LoginDto(
+data class LoginRequest(
     val email: String,
     val password: String
 )
