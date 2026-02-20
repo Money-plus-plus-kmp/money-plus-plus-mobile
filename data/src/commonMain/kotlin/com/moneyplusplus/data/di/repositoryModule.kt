e29e09f6fc6e5febfae9135112cc604c9c5b3d6c
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2026-02-20 16:49:32 +0200

    fix(data): Correct typo in repositoryModule filename

--- data/src/commonMain/kotlin/com/moneyplusplus/data/di/repositoryModule.kt
+++ data/src/commonMain/kotlin/com/moneyplusplus/data/di/repositoryModule.kt
@@ -1,5 +1,6 @@
 package com.moneyplusplus.data.di
 
+import com.moneyplusplus.data.datasource.remote.auth.AuthDataSource
 import com.moneyplusplus.data.datasource.remote.client.createHttpClient
 import com.moneyplusplus.data.repository.AuthRepositoryImpl
 import com.moneyplusplus.domain.repository.AuthRepository
@@ -10,4 +11,5 @@ import org.koin.dsl.module
 internal val repositoryModule = module {
     singleOf(::createHttpClient)
     singleOf(::AuthRepositoryImpl) bind AuthRepository::class
+    singleOf(::AuthDataSource)
 }
